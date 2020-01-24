f19135c91cca56772ca5b2d0dd34aa77db53e203
Zacck Osiemo <coderv63@gmail.com>
2020-01-24 10:31:47 +0200

    Implement recursive exponent calculator

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ fn main() {
         h,
         cannonball(h)
     );
+
+    let m = 3;
+    let n = 5;
+
+    println!("{} raised to power {} is: {}", m, n, raise_to_power(m, n));
 }
 
 fn cannonball(height: i32) -> i32 {
@@ -14,5 +19,14 @@ fn cannonball(height: i32) -> i32 {
     } else {
         // compute and stack items in layers
         height * height + cannonball(height - 1)
+    }
+}
+
+fn raise_to_power(number: i32, power: i32) -> i32 {
+    // check for simple case
+    if power == 0 {
+        1
+    } else {
+        number * raise_to_power(number, power - 1)
     }
 }
